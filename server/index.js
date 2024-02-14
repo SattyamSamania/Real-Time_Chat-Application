92984d66ba5607471ba86910e86dc83f56ccf149
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-14 15:05:11 +0530

    Add Socket.io Logic to Server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,23 @@ const cors = require('cors');
 
 app.use(cors());
 
+
+// TO Create Real-time Connection between Client and Server 
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
+
 app.get('/api', (req, res)=> {
     res.json({message: 'This is Server Running' })
 })
