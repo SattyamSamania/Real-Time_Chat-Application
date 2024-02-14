bff18c1353d72f4d5b2903812baf25cbc5686468
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-14 20:34:17 +0530

    Update Chat Components

--- client/chat-app/components/ChatFooter.jsx
+++ client/chat-app/components/ChatFooter.jsx
@@ -5,7 +5,14 @@ const ChatFooter = () => {
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-    console.log({ userName: localStorage.getItem('userName'), message });
+    if (message.trim() && localStorage.getItem('userName')) {
+      socket.emit('message', {
+        text: message,
+        name: localStorage.getItem('userName'),
+        id: `${socket.id}${Math.random()}`,
+        socketID: socket.id,
+      });
+    }
     setMessage('');
   };
   return (
